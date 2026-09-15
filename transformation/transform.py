dafcbfd23a0222d59784928d810f69b2d563f506
AMINE LEBRINI <aminelebrini4@gmail.com>
2026-09-15 10:13:00 +0100

    i make the transform function and to transform and cleaning my data according to my needs

---
+++ transformation/transform.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import json
+def transformation_data():
+    print("Starting Silver Transformation... !!")
+
+    cities_path = "../data/bronze/my_cities.csv"
+    weather_data = "../data/bronze/weather_data.json"
+
+    new_data_ouput = "../data/silver/clean_weather_data.csv"
+
+    weather_data_array = []
+
+
+    cities_dataframe = pd.read_csv(cities_path)
+
+    rows = []
+
+    with open(weather_data, "r", encoding="utf-8") as file:
+        weather_data_array = json.load(file)
+
+
+    for item in weather_data_array:
+        city_name = item.get("city")
+        daily = item.get("daily", {})
+        dates = daily.get("time", [])
+        temp_max = daily.get("temperature_2m_max", [])
+        temp_min = daily.get("temperature_2m_min", [])
+        precip_sum = daily.get("precipitation_sum", [])
+        precip_prob_max = daily.get("precipitation_probability_max", [])
+        wind_speed_max = daily.get("wind_speed_10m_max",[])
+        wind_gusts_max = daily.get("wind_gusts_10m_max")
+        weather_code = daily.get("weather_code")
+
+        for i in range(len(dates)):
+            rows.append({
+                "city" : city_name,
+                "forecast_date" : dates[i],
+                "temp_max" : temp_max[i],
+                "temp_min" : temp_min[i],
+                "precipitation_sum" : precip_sum[i],
+                "precipitation_probability_max" : precip_prob_max[i],
+                "wind_speed_max" : wind_speed_max[i],
+                "wind_gusts_max" : wind_gusts_max[i],
+                "weather_code" : weather_code[i],
+            })
+
+    weather_data_frame = pd.DataFrame(rows)
+
+    final_data_frame = pd.merge(
+        weather_data_frame,
+        cities_dataframe[["city", "lat", "lng", "admin_name"]],
+        on="city",
+        how="left"
+    )
+
+    # print(final_data_frame)
+
+
+    final_data_frame = final_data_frame.dropna(subset=["forecast_date", "temp_max", "temp_min"])
+    final_data_frame = final_data_frame.sort_values(by=["city", "forecast_date"]).reset_index(drop=True)
+
+    print("succes to transforming and cleaning your data !!!")
+
+    # print(final_data_frame)
+
+    try:
+        final_data_frame.to_csv(new_data_ouput, index=False)
+        print(f"Success! Data imported to {new_data_ouput} !!!")
+    except Exception as e:
+        print(f"Failed to export data: {e}")
+
+
+transformation_data()
+
+
+
+
+    
+    
